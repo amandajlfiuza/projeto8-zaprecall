318764bf256602632aa46f85da635d053132aacd
amandajlfiuza <amandafiuza.pj@gmail.com>
2022-07-23 11:37:35 -0300

    Componentização

---
+++ src/Components/App/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Start from '../Start/Start';
+import Home from '../Home/Home';
+
+export default function App() {
+  return (
+    <>
+      <Start />
+      <Home />
+    </>
+  );
+}
---
+++ src/Components/Start/Start.js
@@ -0,0 +1,11 @@
+import './Start.css';
+
+export function Start() {
+    return (
+        <div class='startScreen'>
+            <img class='logo' />
+            <div class='title'>ZapRecall</div>
+            <div class='startButton'>Iniciar Recall</div>
+        </div>
+    )
+}
