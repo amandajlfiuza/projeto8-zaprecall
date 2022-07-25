4c27ed4b21857f470cac25bc64e1128da83d79c3
amandajlfiuza <amandafiuza.pj@gmail.com>
2022-07-25 09:05:55 -0300

    Mudanças de estado dos flashcards

--- src/Components/Flashcard/Flashcard.js
+++ src/Components/Flashcard/Flashcard.js
@@ -8,17 +8,18 @@ export default function Flashcard({index, question, answer}) {
         <div class={cardClass}>
             <div class='front' onClick={() => setCardClass('open-question flashcard')}>
                 <div class='index-question'>Pergunta {index+1}</div>
-                <ion-icon name="play-outline"></ion-icon>
+                <ion-icon class='icon-play' name="play-outline"></ion-icon>
+                <ion-icon class='icon-incorrect' name="close-circle"></ion-icon>
+                <ion-icon class='icon-correct-with-effort' name="help-circle"></ion-icon>
+                <ion-icon class='icon-correct' name="checkmark-circle"></ion-icon>
             </div>
             <div class='question'>{question}</div>
-            <div onClick={() => setCardClass('open-answer flashcard')}>
-                <img class='turn-icon' src='assets/images/setinha.png'/>
-            </div>
+            <img class='turn-icon' src='assets/images/setinha.png' onClick={() => setCardClass('open-answer flashcard')}/>
             <div class='answer'>{answer}</div>
             <div class='buttons'>
-                <div class='incorrect'>Não lembrei</div>
-                <div class='correct-with-effort'>Quase não lembrei</div>
-                <div class='correct'>Zap!</div>
+                <div class='incorrect' onClick={() => setCardClass('closed incorrect flashcard')}>Não lembrei</div>
+                <div class='correct-with-effort' onClick={() => setCardClass('closed correct-with-effort flashcard')}>Quase não lembrei</div>
+                <div class='correct' onClick={() => setCardClass('closed correct flashcard')}>Zap!</div>
             </div>
         </div>
     )
